d9ea151ca28e28f01e431db5731257c7c85207cd
aminaouini <aminaouini565@gmail.com>
2020-03-14 21:04:49 +0100

    rendered application with dummyData

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -3,6 +3,9 @@
 angular.module('video-player')
   .controller('VideoList', function ($scope) {
     $scope.dummyData = exampleVideoData;
+    // $scope.onClick = function(){
+
+    // }
   })
   .component('videoList', {
     // TODO
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,6 +1,13 @@
 angular.module('video-player')
+  .controller('video-Player', function ($scope) {
+    $scope.data = exampleVideoData[0];
+    $scope.videoLink = "https://www.youtube.com/embed/" + $scope.data.id.videoId;
+    $scope.title = $scope.data.snippet.title;
+    $scope.description = $scope.data.snippet.description;
+  })
 
   .component('videoPlayer', {
     // TODO
+    controller: 'video-Player',
     templateUrl: 'src/templates/videoPlayer.html'
   });
