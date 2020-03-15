b5590c6c154ef5ae7cab9f29a29799becc70e593
aminaouini <aminaouini565@gmail.com>
2020-03-15 22:09:19 +0100

    still working on the get request

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,8 @@
 angular.module('video-player')
-  .controller('App', function (youTube) {
-
-    // console.log(youTube.search)
-
-  })
-
   .component('app', {
     // TODO
-    controller: 'App',
+    // controller: 'App',
     templateUrl: 'src/templates/app.html'
   });
+
+
