ad9b7de2a8c18c1292f97f54dd04911fb573ea6a
aminaouini <aminaouini565@gmail.com>
2020-03-15 21:58:37 +0100

    still working on the get request

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,12 @@
 angular.module('video-player')
+  .controller('App', function (youTube) {
+
+    // console.log(youTube.search)
+
+  })
 
   .component('app', {
     // TODO
+    controller: 'App',
     templateUrl: 'src/templates/app.html'
   });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,17 @@
 angular.module('video-player')
-  .service('youTube', function ($http) {
-    // TODO
+  .service('youTube', function ($http, $log) {
+    this.search = function (searchCategory, cb) {
+      $http.get("https://www.googleapis.com/youtube/v3", {
+        q: searchCategory,
+        maxResults: 5,
+        type: 'video'
+
+      })
+        .then((res) => {
+          cb(res);
+        })
+        .catch((err) => {
+          $log.info(err);
+        });
+    };
   });
